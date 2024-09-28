78bf784c5b2cb395c3e9730d5d393d471df75ceb
PdroFreitass <pedrothdvzs@gmail.com>
2024-09-27 23:32:01 -0300

    dtmoney

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, HeaderContent, NewTransectionbutton,  } from "./styles";
+
+
+
+export function Header() {
+    return(
+        <HeaderContainer>
+            <HeaderContent>
+                <h1>Logo</h1>
+
+
+                <NewTransectionbutton>Nova transation</NewTransectionbutton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
